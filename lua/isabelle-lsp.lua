3601b59d77cc4998de29f966ded7d4d959318be2
Thomas Lindae <thomas@lindae.de>
2025-12-02 15:16:49 +0100

    clarified doc comment

--- lua/isabelle-lsp.lua
+++ lua/isabelle-lsp.lua
@@ -1,6 +1,6 @@
 local M = {}
 
---- Sets vim.g.isabelle_lsp to the given user_config.
+--- Merges the given user_config into vim.g.isabelle_lsp.
 --- @type fun(user_config: isabelle-lsp.Config)
 M.setup = function(user_config)
     --- @type isabelle-lsp.Config
